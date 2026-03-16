483e21089dba162605ef7e60517e37e513eae49c
VipinXRana <vipinranavip97@gmail.com>
2026-03-16 13:50:50 +0530

    🧠😎

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from './pages/Home'
+import ListMovies from "./pages/ListMovies";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/listmovies" element={<ListMovies />} />
     </Routes>
   );
 };
---
+++ admin/src/components/ListMoviesPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ListMoviesPage = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ListMoviesPage
