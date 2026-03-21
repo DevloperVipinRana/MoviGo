489a5ec3130519f8411ffda5aec090cd835af606
VipinXRana <vipinranavip97@gmail.com>
2026-03-21 22:18:54 +0530

    admin about to complete🔥🥵

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from './pages/Home'
 import ListMovies from "./pages/ListMovies";
+import Dashboard from "./pages/Dashboard";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/listmovies" element={<ListMovies />} />
+      <Route path="/dashboard" element={<Dashboard />} />
     </Routes>
   );
 };
